e732ea37fc98f28bf63a1580be511975ea790377
Denise Buttigieg <denise.buttigieg.14@um.edu.mt>
2018-02-18 18:37:00 +0100

    working on receiving enquiry form

--- js/taxi-inquiry.js
+++ js/taxi-inquiry.js
@@ -2,7 +2,7 @@
 
 $(function() {
 
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    $("#bookTransfer input,#bookTransfer textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
@@ -11,22 +11,58 @@ $(function() {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
+            var surname = $("input#surname").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
+            var oneWayTransferBtn = $("input#onewaytrf:checked").val();
+            var returnTransferBtn = $("input#returntrf:checked").val();
+            var trf1_transferType = $("input#transferType").val();
+            var trf1_numAdults = $("input#numadults").val();
+            var trf1_numChildren = $("input#numchild").val();
+            var trf1_datetime = $("input#datetime").val();
+            var trf1_pickup = $("input#pickup").val();
+            var trf1_dropoff = $("input#dropoff").val();
+
+            var trf2_transferType = $("input#transferTypeRet").val();
+            var trf2_numAdults = $("input#numadultsRet").val();
+            var trf2_numChildren = $("input#numchildRet").val();
+            var trf2_datetime = $("input#datetimeret").val();
+            var trf2_pickup = $("input#pickupret").val();
+            var trf2_dropoff = $("input#dropoffret").val();
+
+            var comments = $("textarea#comments").val();
+
+            var isReturn;
+
+            if (oneWayTransferBtn) {
+                isReturn = "No"
+            } else {
+                isReturn = "Yes"
             }
+
             $.ajax({
-                url: "././mail/trasfer_inquiry.php",
+                url: "././mail/transfer_enquiry.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+                    surname: surname,
                     email: email,
-                    message: message
+                    phone: phone,
+                    isReturn: isReturn,
+                    returnTransferBtn: returnTransferBtn,
+                    trf1_transferType: trf1_transferType,
+                    trf1_numAdults: trf1_numAdults,
+                    trf1_numChildren: trf1_numChildren,
+                    trf1_datetime: trf1_datetime,
+                    trf1_pickup: trf1_pickup,
+                    trf1_dropoff: trf1_dropoff,
+                    trf2_transferType: trf2_transferType,
+                    trf2_numAdults: trf2_numAdults,
+                    trf2_numChildren: trf2_numChildren,
+                    trf2_datetime: trf2_datetime,
+                    trf2_pickup: trf2_pickup,
+                    trf2_dropoff: trf2_dropoff,
+                    addcomments: comments
                 },
                 cache: false,
                 success: function() {
@@ -35,7 +71,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you for your enquiry. We hope you to see you soon. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -47,7 +83,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that my mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
